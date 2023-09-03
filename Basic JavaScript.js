680f5d96cdffc7b00e82d7208ee08c571f7e83fc
Bluecode77732 <doookuuu@gmail.com>
2023-09-03 20:41:17 +0900

    Quoting Strings with Single Quotes

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -5,5 +5,8 @@ function Function1(thisParamIsTrue) {
     return "No, it's not."
 }
 
-const myStr = "'I' am a liberal-conservatist, who believes in and belonging from the \"Classic Liberalism\".";
-console.log(myStr);
+const myStr1 = "'I' am a liberal-conservatist, who believes in and belonging from the \"Classic Liberalism\".";
+console.log(myStr1);
+
+const myStr2 = "<a href=\"http://www.example.com\" target=\"_blank\">Link</a>";
+console.log(myStr2);
