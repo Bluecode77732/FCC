ab11333c54e8bcbd184bd062d963d3f1391d7ff5
Bluecode77732 <doookuuu@gmail.com>
2023-09-03 20:33:10 +0900

    Escaping Literal Quotes in Strings

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -4,3 +4,6 @@ function Function1(thisParamIsTrue) {
     }
     return "No, it's not."
 }
+
+const myStr = "'I' am a liberal-conservatist, who believes in and belonging from the \"Classic Liberalism\".";
+console.log(myStr);
