f3ed378d0e4421f917aefb93a1a9546db4b06b6f
Bluecode77732 <doookuuu@gmail.com>
2023-09-03 23:58:45 +0900

    Escape Sequences in Strings

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -5,8 +5,30 @@ function Function1(thisParamIsTrue) {
     return "No, it's not."
 }
 
+
+
 const myStr1 = "'I' am a liberal-conservatist, who believes in and belonging from the \"Classic Liberalism\".";
 console.log(myStr1);
 
-const myStr2 = "<a href=\"http://www.example.com\" target=\"_blank\">Link</a>";
+
+
+const myStr2 = '<a href="http://www.example.com" target="_blank">Link</a>';
 console.log(myStr2);
+
+
+
+/* Escape Sequences in Strings */
+
+/**
+ * Code	  Output
+    \'	  single quote
+    \"	  double quote
+    \\	  backslash
+    \n	  newline
+    \t	  tab
+    \r	  carriage return
+    \b	  backspace
+    \f	  form feed
+*/
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+console.log(myStr);
