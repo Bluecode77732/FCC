5f0ac77a82804ec6aaa97d4a161058344efcdae6
Bluecode77732 <doookuuu@gmail.com>
2023-09-04 22:16:31 +0900

    Nest one Array within Another Array; Multi Dimentional Array

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -77,3 +77,10 @@ const secondToLastLetterOfLastName = lastName2[lastName2.length - 2];
 
 
 
+/** Nest one Array within Another Array = Multi-Dimentional Array
+ * You can also nest arrays within other arrays, like below:
+ * 
+ * const teams = [["Bulls", 23], ["White Sox", 45]];
+ * 
+ * This is also called a 'multi-dimensional array'.
+*/
