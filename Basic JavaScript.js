b21b1441b9c82a93d4480e3bae2050b0ca2db59a
Bluecode77732 <doookuuu@gmail.com>
2023-09-20 23:02:20 +0900

    Profile Lookup

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -915,3 +915,64 @@ sum([2, 3, 4, 5], 3); //Log : 9
 
 
 
+/** Profile Lookup
+ *  
+ * A lookUpProfile function that takes name and a property (prop) as arguments has been pre-written for you.
+
+ * The function should check if name is an actual contact's firstName 
+   and the given property (prop) is a property of that contact.
+ * 
+ * If both are true, then return the "value" of that property.
+ * 
+ * If 'name' does not correspond to any contacts then return the string No such contact.
+ * 
+ * If 'prop' does not correspond to any valid properties of a contact found 
+   to match 'name' then return the string No such property.
+*/
+
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+  if(name === contacts[firstName] && prop === contacts[likes]) {
+    return contacts[prop];
+
+  } else if(name !== contacts[name]) {
+    return "No such contact";
+
+  } else if(prop !== contacts[prop]) {
+    return "No such property";
+  }
+  // Only change code above this line
+}
+
+lookUpProfile("Akira", "likes");
+
+
+
