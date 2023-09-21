d0588d50f0d5e3978a4bcc9b3b6dea80ec08caea
Bluecode77732 <doookuuu@gmail.com>
2023-09-21 22:59:47 +0900

    Use Multiple Conditional (Ternary) Operators

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -1124,3 +1124,32 @@ function checkEqual(a, b) {
 }
 
 checkEqual(1, 2);
+
+
+
+/** Use Multiple Conditional (Ternary) Operators
+ * In the previous challenge, you used a single conditional operator. 
+ * You can also chain them together to check for multiple conditions. 
+ * It is considered best practice to format multiple conditional operators 
+   such that each condition is on a separate line, as shown above. 
+   Using multiple conditional operators without proper indentation may make your code hard to read. 
+   
+ * For example:
+
+ * function findGreaterOrEqual(a, b) {
+ *   return (a === b) ? "a and b are equal" : (a > b) ? "a is greater" : "b is greater";
+ * }
+
+ * In the checkSign function, use multiple conditional operators - 
+   following the recommended format used in findGreaterOrEqual - 
+   to check if a number is positive, negative or zero. The function should return positive, negative or zero.
+*/
+
+function checkSign(num) {
+  return (num === 0) ? "zero" : (num > 0) ? "positive" : "negative";
+}
+
+checkSign(10);
+
+
+
