ca5328d48a00c22af8f5bf6fad13071483809d96
Bluecode77732 <doookuuu@gmail.com>
2023-09-21 22:27:12 +0900

    Generate Random Fractions with JavaScript, Generate Random Whole Numbers with JavaScript

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -993,3 +993,56 @@ lookUpProfile("Akira", "likes");
 
 
 
+
+/** Generate Random Fractions with JavaScript
+ * Random numbers are useful for creating random behavior.
+  
+ * JavaScript has a 'Math.random()' function that generates a random decimal number between 0 (inclusive) and 1 (exclusive). 
+ * Thus 'Math.random()' can return a 0 but never return a 1. 
+  
+ * Note: Like Storing Values with the Assignment Operator, 
+   all function calls will be resolved before the return executes, 
+   so we can return the value of the Math.random() function. 
+ 
+ * Change randomFraction to return a random number instead of returning 0.
+*/
+
+function randomFraction() {
+
+  // Only change code below this line
+
+  return Math.random();
+
+  // Only change code above this line
+}
+
+
+
+/** Generate Random Whole Numbers with JavaScript
+ * You can generate random decimal numbers with Math.random(), 
+   but sometimes you need to generate random whole numbers. 
+ * The following process will give you a random whole number less than 20:
+  
+ * Use Math.random() to generate a random decimal number.
+ * Multiply that random decimal number by 20.
+ * Use Math.floor() to round this number down to its nearest whole number.
+ * Remember that Math.random() can never quite return a 1, 
+   so it's impossible to actually get 20 since you are rounding down with Math.floor(). 
+ * This process will give you a random whole number in the range from 0 to 19.
+  
+ * Putting everything together, this is what your code looks like:
+
+ * Math.floor(Math.random() * 20);
+ * You are calling Math.random(), multiplying the result by 20, 
+   then passing the value to Math.floor() to round the value down to the nearest whole number.
+  
+ * Use this technique to generate and return a random whole number in the range from 0 to 9.
+*/
+
+function randomWholeNum() {
+  return Math.floor(Math.random() * 10);
+}
+
+
+
+
